a1947fb915eb6e19b222cd45e14930c934eee937
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-01-10 23:17:20 +0530

    Update updatedata.php

---
+++ updatedata.php
@@ -0,0 +1,21 @@
+<?php
+include 'config.php'; // Include the correct database connection file
+
+if(isset($_POST['sid']) && isset($_POST['sname']) && isset($_POST['saddress']) && 
+isset($_POST['sclass']) && isset($_POST['sphone']))
+{
+    $sid = $_POST['sid'];
+    $sname = $_POST['sname'];
+    $saddress = $_POST['saddress'];
+    $sclass = $_POST['sclass'];
+    $sphone = $_POST['sphone'];
+
+    $sql = "UPDATE students SET name='{$sname}', address='{$saddress}', class='{$sclass}', phone='{$sphone}' WHERE id={$sid}";
+
+    $result = mysqli_query($conn, $sql) or die("Query Unsuccessful: " . mysqli_error($conn));
+
+    header("Location: index.php");
+}
+
+mysqli_close($conn);
+?>
