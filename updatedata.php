eb8086a8e552cbde32296b43f922ab4f510b0d36
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-02-13 18:40:04 +0530

    Update updatedata.php

--- updatedata.php
+++ updatedata.php
@@ -1,18 +1,21 @@
 <?php
 include 'config.php'; // Include the correct database connection file
 
-if(isset($_POST['sid']) && !empty($_POST['sid']) && isset($_POST['sname']) && isset($_POST['saddress']) && 
-isset($_POST['sclass']) && isset($_POST['sphone']))
-{
+if (
+    isset($_POST['sid']) && !empty($_POST['sid']) && isset($_POST['sname']) && isset($_POST['saddress']) &&
+    isset($_POST['sclass']) && isset($_POST['sphone'])
+) {
     $sid = $_POST['sid'];
     $sname = $_POST['sname'];
     $saddress = $_POST['saddress'];
     $sclass = $_POST['sclass'];
     $sphone = $_POST['sphone'];
 
-    $sql = "UPDATE students SET name='{$sname}', address='{$saddress}', class='{$sclass}', phone='{$sphone}' WHERE sid={$sid}"; // Use the correct column name
+    $sql = "UPDATE students SET name='{$sname}', address='{$saddress}', 
+    class='{$sclass}', phone='{$sphone}' WHERE sid={$sid}"; // Use the correct column name
 
-    $result = mysqli_query($conn, $sql) or die("Query Unsuccessful: " . mysqli_error($conn));
+    $result = mysqli_query($conn, $sql) or 
+    die("Query Unsuccessful: " . mysqli_error($conn));
 
     // Redirect to index.php after successful update
     header("Location: index.php");
@@ -20,4 +23,3 @@ isset($_POST['sclass']) && isset($_POST['sphone']))
 }
 
 mysqli_close($conn);
-?>
