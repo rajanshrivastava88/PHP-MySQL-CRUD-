5ce354f64f4dac7d5b7d1ff2191aef673e325269
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-02-13 18:40:41 +0530

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -9,9 +9,11 @@ if(isset($_POST['sname']) && isset($_POST['saddress']) && isset($_POST['class'])
     $sphone = $_POST['sphone'];
 
     // Use the correct column names from your database table
-    $sql = "INSERT INTO students (sname, saddress, sclass, sphone) VALUES ('{$sname}', '{$saddress}', '{$sclass}', '{$sphone}')";
+    $sql = "INSERT INTO students (sname, saddress, sclass, sphone) VALUES ('{$sname}', 
+    '{$saddress}', '{$sclass}', '{$sphone}')";
 
-    $result = mysqli_query($conn, $sql) or die("Query Unsuccessful: " . mysqli_error($conn));
+    $result = mysqli_query($conn, $sql) or 
+    die("Query Unsuccessful: " . mysqli_error($conn));
 
     // Redirect to index.php after successful insertion
     header("Location: index.php");
