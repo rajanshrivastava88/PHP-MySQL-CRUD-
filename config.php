9dac6c48589c978242f9b81c401c0db932e19e25
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-02-13 18:43:00 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "crud_operations";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname, port: "3307");
+$conn = mysqli_connect($servername, $username, $password, 
+$dbname, port: "3307");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
